fedfec84e834e11b3968f865283f0cdd0b0f850a
Aubrey Monaco <aublmonaco@gmail.com>
2025-09-18 15:37:38 -0400

    Committing Final CS4

--- monaco_aubrey_casestudy4.R
+++ monaco_aubrey_casestudy4.R
@@ -18,20 +18,22 @@ joined_flights_airports <- left_join(JFK_flights, airports)
 farthest_JFK <- slice_head(joined_flights_airports)
 View(farthest_JFK)
 
-joined_flights_airports <- joined_flights_airports %>%
-  rename(dest = faa) %>%
-  rename(dest_name = name)
-
 farthest_JFK <- farthest_JFK %>%
   rename(dest = faa,
          dest_name = name)
 
+airports_filtered <- airports %>% inner_join(joined_flights_airports)
+
+joined_flights_airports <- joined_flights_airports %>%
+  rename(dest = faa,
+         dest_name = name)
+
 farthest_airport <- farthest_JFK$dest_name
-sf_airports <- st_as_sf(airports, coords = c("lon", "lat"), crs = 4326)
+sf_airports <- st_as_sf(airports_filtered, coords = c("lon", "lat"), crs = 4326)
 st_crs(sf_airports)
 
-JFK_airport <- filter(sf_airports, faa == "JFK")
+JFK_airport <- filter(airports, faa == "JFK")
 
-airport_map <- ggplot() + geom_sf(data = world) + geom_sf(data = sf_airports) + geom_sf(data = JFK_airport, aes(color = "red")) + geom_point(data = farthest_JFK, aes(x = lon, y = lat, color = "blue"))
+airport_map <- ggplot() + geom_sf(data = world) + geom_sf(data = sf_airports) + geom_point(data = JFK_airport, aes(x = lon, y = lat, color = "red")) + geom_point(data = farthest_JFK, aes(x = lon, y = lat, color = "blue")) + labs(title = "Farthest Airport From JFK", x = "Longitude", y = "Latitude", color = "Airports Key") + scale_color_hue(labels = c("blue" = "HLN", "red" = "JFK"))
 
 airport_map + coord_sf()
