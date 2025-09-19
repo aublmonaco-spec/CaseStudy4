0b6b09c3d2d3b6dbab5093f3807e04f0495ef408
Aubrey Monaco <aublmonaco@gmail.com>
2025-09-19 09:37:57 -0400

    FINAL FINAL COMMIT; Fixed legend and colors

--- monaco_aubrey_casestudy4.R
+++ monaco_aubrey_casestudy4.R
@@ -24,7 +24,7 @@ joined_flights_airports <- left_join(JFK_flights, airports)
 farthest_JFK <- slice_head(joined_flights_airports)
 View(farthest_JFK)
 
-#This step is revertin the names in the "farthest_JFK" data for clarity 
+#This step is reverting the names in the "farthest_JFK" data for clarity 
 farthest_JFK <- farthest_JFK %>%
   rename(dest = faa,
          dest_name = name)
@@ -48,8 +48,7 @@ st_crs(sf_airports)
 #This step created a point to highlight the JFK airport
 JFK_airport <- filter(airports, faa == "JFK")
 
-#For this map, I used geom_sf for the basemap and the "sf_airports" data, since they are spatial, then I used geom_point for the JFK_airport and farthest_JFK data. I specified for the point layers that the x-axis is based on longitude and the y-axis is based on latitude. I also added labels to the legend. Scale_color_hue allowed me to change the labels within the legend. 
-airport_map <- ggplot() + geom_sf(data = world) + geom_sf(data = sf_airports, aes(color = "black")) + geom_point(data = JFK_airport, aes(x = lon, y = lat, color = "blue")) + geom_point(data = farthest_JFK, aes(x = lon, y = lat, color = "red")) + labs(title = "Global Airports With NYC Origins", x = "Longitude", y = "Latitude", color = "Airport Type") + scale_color_hue(labels = c("black" = "Other Destinations", "blue" = "JFK", "red" = "Farthest Airport"))
-
+#For this map, I used geom_sf for the basemap and the "sf_airports" data, since they are spatial, then I used geom_point for the JFK_airport and farthest_JFK data. I specified for the point layers that the x-axis is based on longitude and the y-axis is based on latitude. I also added labels to the legend. Scale_color_identity allowed me to change the labels within the legend. 
+airport_map <- ggplot() + geom_sf(data = world) + geom_sf(data = sf_airports, aes(color = "black")) + geom_point(data = JFK_airport, aes(x = lon, y = lat, color = "blue")) + geom_point(data = farthest_JFK, aes(x = lon, y = lat, color = "red")) + labs(title = "Global Airports With NYC Origins", x = "Longitude", y = "Latitude", colour = "Airport Type") + scale_color_identity(breaks = c("black", "blue", "red"), labels = c("Other Destinations", "JFK", "Farthest Airport"), guide = "legend")
 #This step helped create a proper map view of my map
 airport_map + coord_sf()
